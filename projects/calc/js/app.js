7134dd91a8d302127ddd12ff758991df1a450bc1
toshew <vladantosev@yahoo.com>
2019-05-05 17:58:06 +0200

    Added function showResult

--- projects/calc/js/app.js
+++ projects/calc/js/app.js
@@ -7,6 +7,7 @@ const plusMinus = document.getElementById('plus-minus');
 
 // Event listeners
 document.body.addEventListener("click", addOnDisplay1);
+showTotal.addEventListener("click", showResult); 
 
 // Functions
 function addOnDisplay1(e) {
@@ -14,4 +15,15 @@ function addOnDisplay1(e) {
     display1.textContent += e.target.value;
     e.target.blur();
   }
+}
+
+function showResult(e) {
+  if (display1.textContent.includes('%')) {
+    let percent = display1.textContent.replace('%', '*(1/100)');
+    display.textContent = eval(percent);
+    e.target.blur();
+  } else {
+    display.textContent = eval(display1.textContent);
+    e.target.blur();
+  }        
 }
